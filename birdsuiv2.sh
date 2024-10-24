2a0970cf234f28fed0998c54c9b6119c4e328fc2
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-24 17:29:48 +0900

    Update birdsuiv2.sh

--- birdsuiv2.sh
+++ birdsuiv2.sh
@@ -20,7 +20,7 @@ echo -e "${CYAN}웜캐칭 / 에그업그레이드/ 에그크래킹 / 각종 태�
 
 echo -e "${GREEN}설치 옵션을 선택하세요:${NC}"
 echo -e "${YELLOW}1. Bird 봇 새로 설치${NC}"
-echo -e "${YELLOW}2. 기존정보 그대로 이용하기(재실행)${NC}"
+echo -e "${YELLOW}2. 재실행하기${NC}"
 read -p "선택: " choice
 
 case $choice in
